46c8426de47ec76bf9e865ab25b02de3620843a9
Adnan Haque <adnan007.id@gmail.com>
2023-09-08 13:31:24 +0600

    Add module status table

---
+++ Extension/src/components/module-status-table.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	type Status = 'positive'|'negative'|'warning'|'neutral';
+	type StatusObject= {
+		status: Status,
+		message?: string
+	};
+	type Modules = 'criticalCss'|'deferredJs'|'lazyLoading'|'concatenateJs'|'concatenateCss'|'imageCdn';
+	
+	export let moduleStatus: Record< Modules, StatusObject >;
+
+	const getModuleStatusMessage = (statusObj: StatusObject) => {
+		const contextualMessages = {
+			'positive': 'Serving',
+			'negative': 'Not Serving',
+			'neutral': 'Ok',
+			'warning': 'Undetermined',
+		}
+		return statusObj.message || contextualMessages[statusObj.status]
+	}
+</script>
+
+<table class="module-status-table">
+	<thead>
+		<tr>
+			<th>Module</th>
+			<th>Status</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class={moduleStatus.criticalCss.status}>
+			<td class="module-name">Critical CSS</td>
+			<td class="module-status">{getModuleStatusMessage(moduleStatus.criticalCss)}</td>
+		</tr>
+		<tr class={moduleStatus.deferredJs.status}>
+			<td class="module-name">Deferred JS</td>
+			<td class="module-status">{getModuleStatusMessage(moduleStatus.deferredJs)}</td>
+		</tr>
+		<tr class={moduleStatus.lazyLoading.status}>
+			<td class="module-name">Lazy Loading</td>
+			<td class="module-status">{getModuleStatusMessage(moduleStatus.lazyLoading)}</td>
+		</tr>
+		<tr class={moduleStatus.concatenateCss.status}>
+			<td class="module-name">Concatenate CSS</td>
+			<td class="module-status">{getModuleStatusMessage(moduleStatus.concatenateCss)}</td>
+		</tr>
+		<tr class={moduleStatus.concatenateJs.status}>
+			<td class="module-name">Concatenate JS</td>
+			<td class="module-status">{getModuleStatusMessage(moduleStatus.concatenateJs)}</td>
+		</tr>
+		<tr class={moduleStatus.imageCdn.status}>
+			<td class="module-name">Image CDN</td>
+			<td class="module-status">{getModuleStatusMessage(moduleStatus.imageCdn)}</td>
+		</tr>
+	</tbody>
+</table>
+
+<style lang="scss">
+	.module-status-table {
+		width: 100%;
+
+		thead th {
+				text-align: left;
+		}
+	}
+	.module-status {
+		border: 1px solid black;
+		padding: 4px;
+
+		.positive & {
+			background-color: green;
+			color: white;
+		}
+
+		.negative & {
+			background-color: red;
+			color: white;
+		}
+
+		.neutral & {
+			background-color: transparent;
+		}
+
+		.warning & {
+			background-color: yellow;
+		}
+	}
+</style>
--- Extension/src/popup/popup.svelte
+++ Extension/src/popup/popup.svelte
@@ -1,14 +1,21 @@
-<script lang="ts">
-  console.log('hello from the popup!');
+<script lang=ts>
+	import ModuleStatusTable from "../components/module-status-table.svelte";
 </script>
-
-<div>
-  <h1>My New Web Extension</h1>
+<div class="jetpack-boost-debugger">
+	<h1>Jetpack Boost Debugger</h1>
+	<ModuleStatusTable moduleStatus={{
+		criticalCss: { status: 'positive' },
+		lazyLoading: { status: 'negative' },
+		deferredJs: { status: 'positive' },
+		concatenateJs: { status: 'warning' },
+		concatenateCss: { status: 'neutral' },
+		imageCdn: { status: 'warning', message: "I'm sorry, I have no idea :'("}
+	}} />
 </div>
 
 <style>
-  div {
-    display: flex;
-    justify-content: center;
-  }
+	.jetpack-boost-debugger {
+		width: 600px;
+		max-width: 100%;
+	}
 </style>
