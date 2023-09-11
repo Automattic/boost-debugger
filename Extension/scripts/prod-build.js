1238114f7f530f5c0cae2c6ee9253098e778c5c3
Adnan Haque <adnan007.id@gmail.com>
2023-09-11 14:07:47 +0600

    Make build hasslefree

--- Extension/scripts/prod-build.js
+++ Extension/scripts/prod-build.js
@@ -1,12 +1,15 @@
-const fs = require('fs/promises');
+const fsPromises = require('fs/promises');
+const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 main();
 
 async function main() {
-  await fs.rm('./dist', { recursive: true });
-  await fs.mkdir('./dist');
+  if (fs.existsSync('./dist')) {
+    await fsPromises.rm('./dist', { recursive: true });
+  }
+  await fsPromises.mkdir('./dist');
   await exec('npm run build -- --prod');
   console.log('Done');
 }
