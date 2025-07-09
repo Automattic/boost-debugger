dc93fe39452b7e9ec892325f8631c800f8e89d0e
Peter Petrov <peter.petrov89@gmail.com>
2025-07-09 17:30:16 +0300

    Update LCP module name to match the one in the Boost UI

--- Extension/src/modules/lcp.ts
+++ Extension/src/modules/lcp.ts
@@ -3,7 +3,7 @@ import type { Module } from "./interface-module";
 
 export class Lcp implements Module {
 	public readonly identifier = 'lcp';
-	public readonly label = 'LCP';
+	public readonly label = 'Optimize LCP Images';
 
 	public getStatus(): StatusObject {
 		const lcpAttr = document.querySelector('[data-jp-lcp-optimized]');
